1c9bf1ed833453aeabb356642442fadd6916bb64
jonkrause <jonathonkrause@gmail.com>
2018-05-11 15:15:53 -0400

    first commit. simple setup

---
+++ src/components/desrever.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="hello">
+    <h1>{{ title }}</h1>
+    <textarea v-model="tempText" rows="10" cols="60" placeholder="Text goes here..."></textarea>
+    <br>
+    <button @click="setText">reverse it</button> <button @click="alertCopy" v-clipboard:copy="finalText">copy text</button>
+    <br>
+    <p>{{ tempText }}</p>
+    <hr>
+    <p>{{ finalText }}</p>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'desrever',
+  data() {
+    return {
+      title: 'DESREVER',
+      tempText: '',
+      finalText: ''
+    }
+  },
+  methods: {
+    setText: function() {
+      this.finalText = this.tempText
+        .split('')
+        .reverse()
+        .join('')
+    },
+    alertCopy() {
+      this.$swal({
+        text: 'copied to clipboard',
+        timer: 1500,
+        showConfirmButton: false,
+        backdrop: `rgba(0, 0, 123, 0.5)`
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+h1,
+h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,17 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import VueClipboard from 'vue-clipboard2'
+import VueSweetAlert2 from 'vue-sweetalert2'
+import App from './App'
+
+Vue.use(VueClipboard)
+Vue.use(VueSweetAlert2)
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  components: { App },
+  template: '<App/>'
+})
